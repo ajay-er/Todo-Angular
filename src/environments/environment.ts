b8e9d76901affe0fccf4ddc67e0a62795b48175b
Ajay <ajayzyan100@gmail.com>
2023-07-09 09:23:16 +0530

    firebase config added

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+
+        apiKey: "AIzaSyBEif8Z-F2wPpRU2C7VdKityzBxGVwnHRE",
+      
+        authDomain: "todo-angular-fd8a8.firebaseapp.com",
+      
+        projectId: "todo-angular-fd8a8",
+      
+        storageBucket: "todo-angular-fd8a8.appspot.com",
+      
+        messagingSenderId: "261031538462",
+      
+        appId: "1:261031538462:web:0faa5653a25e1c7957e30f"
+      
+      }
+      
+  };
